9b9b0aafd7a778ddcca01a0604c8e9b5ddb7c68d
ryuki8643 <garyuki88me@gmail.com>
2022-03-30 17:43:12 +0900

    modify style

--- src/component/home/component/shareIcons.tsx
+++ src/component/home/component/shareIcons.tsx
@@ -9,41 +9,43 @@ import {
 } from "react-share";
 import * as React from "react";
 
+let shareText="You can visualize data of SDGs indicators in a map"
+let shareUrl="https://eco-map-dev-kapikei.web.app/"
 type ShareIconType={
     top:string
     left:string
 }
 export function ShareIcons(props:ShareIconType){
     return (
-        <Box sx={{height:'20%',wedth:'100%',transform:'scale(1.2)',marginLeft:props.left+'%',marginTop:props.top+'%'}}>
+        <Box sx={{height:'20%',width:'85%',transform:'scale(1.2)',marginLeft:props.left+'%',marginTop:props.top+'%'}}>
             <FacebookShareButton
-                url={"https://eco-map-dev-kapikei.web.app/"}
-                quote={"フェイスブックはタイトルが付けれるようです"}
-                hashtag={"#hashtag"}
+                url={shareUrl}
+                quote={shareText}
+                hashtag={"#EcoMap"}
 
                 className="Demo__some-network__share-button"
             >
                 <FacebookIcon size={32} round />
             </FacebookShareButton>
             <TwitterShareButton
-                title={"test"}
-                url={"https://eco-map-dev-kapikei.web.app/"}
-                hashtags={["hashtag1", "hashtag2"]}
+                title={shareText}
+                url={shareUrl}
+                hashtags={["EcoMap"]}
             >
                 <TwitterIcon size={32} round />
 
             </TwitterShareButton>
             <LinkedinShareButton
-                title={"test"}
-                url={"https://eco-map-dev-kapikei.web.app/"}
+                title={shareText}
+                url={shareUrl}
 
             >
                 <LinkedinIcon size={32} round />
 
             </LinkedinShareButton>
             <WhatsappShareButton
-                title={"test"}
-                url={"https://eco-map-dev-kapikei.web.app/"}
+                title={shareText}
+                url={shareUrl}
 
             >
                 <WhatsappIcon size={32} round />
